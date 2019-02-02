38e3bd70b86f6bcc5d50a01c81f974ae496a0e1c
Pratyush Das <reikdas@gmail.com>
2019-02-02 19:36:54 +0530

    Fixed perceptron

--- perceptron.py
+++ perceptron.py
@@ -3,34 +3,46 @@ from sklearn.datasets import load_iris
 
 class Perceptron(object):
 
-    def __init__(self, input_size, lr=1, epochs=100):
-        self.W = np.zeros(input_size + 1)
+    def __init__(self, shape, learning_rate=0.1, epochs=50):
+        self.weight = np.random.randn(1, shape + 1)
         self.epochs = epochs
-        self.lr = lr
+        self.learning_rate = learning_rate
 
-    def activation_fn(self, x):
-        return 1 if x >= 0 else 0
+    def activation(self, X):
+        prediction = []
+        for elem in X:
+            if elem>=0.5:
+                prediction.append(1)
+            else:
+                prediction.append(0)
+        prediction = np.asarray(prediction)
+        return prediction
 
-    def predict(self, x):
-        z = self.W.T.dot(x)
-        a = self.activation_fn(z)
+    def predict(self, X):
+        z = np.dot(X, self.weight[:, 1:].T) + self.weight[0, 0]
+        a = self.activation(z)
         return a
 
     def fit(self, X, d):
+        self.errors = []
         for epoch in range(self.epochs):
-            err = 0
-            for i in range(d.shape[0]):
-                x = np.insert(X[i], 0, 1)
-                y = self.predict(x)
-                e = d[i] - y
-                err += e
-                self.W = self.W + self.lr*e*x
-            print("epoch=%d, lrate=%.2f, error=%.2f" % (epoch, self.lr, err))
+           y = self.predict(X)
+           e = d - y
+           mean_error = np.mean(e)
+           self.weight[:, 1:] += (self.learning_rate*np.dot(X.T, e)).T
+           self.weight[:, 0] += self.learning_rate*mean_error
+           err = mean_error**2
+           self.errors.append(err)
+           print("epoch=%d, error=%.2f" % (epoch, err))
 
 if __name__ == "__main__":
     iris = load_iris()
     X = iris.data
+    X_train = X[:90]
+    X_test = X[90:100]
     y = iris.target
+    y_train = y[:90]
+    y_test = y[90:100]
 
-    perceptron = Perceptron(input_size=4)
-    perceptron.fit(X, y)
+    perceptron = Perceptron(shape=X.shape[1])
+    perceptron.fit(X_train, y_train)
